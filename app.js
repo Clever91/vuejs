7f99fc47ea7326e0437c13e95c2cf2f4adeb129b
Sherzod Usmonov <sherzod.usmon.91@gmail.com>
2019-09-27 19:41:38 +0500

    converted html to Vue component

--- app.js
+++ app.js
@@ -1,41 +1,88 @@
-var app = new Vue({
-  el: "#app",
-  data: {
-    product: {
-      productId: 123,
-      title: "Sock",
-      price: 26000,
-      images: [
-        {
-          id: 12,
-          name: "Green Sock",
-          url: "./images/sock-green.jpg",
-          quality: 10,
-          style: {
-            backgroundColor: 'green',
-            width: '40px',
-            height: '40px',
-            margin: '5px',
-            border: '1px solid black',
+Vue.component("say", {
+  props: ['message'],
+  template: `
+    <div>
+      <h3> {{ message }} </h3>
+    </div>
+  `,
+  data() {
+    return {}
+  }
+});
+
+// Define a new component called button-counter
+Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+});
+
+
+// create product component
+Vue.component("product", {
+  template: `
+    <div>
+      <h1>{{ title }}</h1>
+      <h3>Cart ( {{ cart.length }} )</h3>
+      <img :src="image"
+          alt="Green Sock"
+          width="500px"
+          height="500px">
+
+      <div v-for="(image, index) in product.images"
+          :key="image.id"
+          @mouseover="updateProductImage(index, image.url)"
+          class="color-box"
+          :style="image.style"></div>
+
+      <button type="button"
+            name="button"
+            v-on:click="addToCart(product)"
+            :disabled="inStock"
+            :class="{ disabledButton: !product.inStock }">Add Cart</button>
+    </div>
+  `,
+  data() {
+    return {
+      product: {
+        productId: 123,
+        title: "Sock",
+        price: 26000,
+        images: [
+          {
+            id: 12,
+            name: "Green Sock",
+            url: "./images/sock-green.jpg",
+            quality: 10,
+            style: {
+              backgroundColor: 'green',
+              width: '40px',
+              height: '40px',
+              margin: '5px',
+              border: '1px solid black',
+            },
           },
-        },
-        {
-          id: 13,
-          name: "White Sock",
-          url: "./images/sock-white.jpg",
-          quality: 0,
-          style: {
-            backgroundColor: 'white',
-            width: '40px',
-            height: '40px',
-            margin: '5px',
-            border: '1px solid black',
-          },
-        }
-      ],
-      selectedIndex: 0
-    },
-    cart: [],
+          {
+            id: 13,
+            name: "White Sock",
+            url: "./images/sock-white.jpg",
+            quality: 0,
+            style: {
+              backgroundColor: 'white',
+              width: '40px',
+              height: '40px',
+              margin: '5px',
+              border: '1px solid black',
+            },
+          }
+        ],
+        selectedIndex: 0
+      },
+      cart: [],
+    }
   },
   methods: {
     updateProductImage(index, url) {
@@ -57,4 +104,9 @@ var app = new Vue({
       return this.product.images[this.product.selectedIndex].quality > 0;
     }
   }
+});
+
+
+var app = new Vue({
+  el: "#app"
 });
